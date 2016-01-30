f309574dcdbfa07432be669dd52cfbd91a70a93e
B.E. Henriksen <e@jiku.ca>
2016-01-30 15:27:24 +0100

    Adds errors to README.

---
+++ README.md
@@ -0,0 +1,16 @@
+# STATUS
+
+Core `stylus` causes
+
+```
+=> Errors prevented startup:
+
+   While loading plugin `compileStylusBatch` from package `stylus`:
+   error: Can't load npm module 'babel-runtime/helpers/slicedToArray' from packages/modules-runtime.js. Check your Npm.depends().
+```
+
+Core LESS causes (on client)
+
+```
+Error: Cannot find module '/imports/styles/index.less' on client
+```
--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import * as StylesCSS from "/imports/styles/index.css"
 import * as StylesSASS from "/imports/styles/index.sass"
-import * as StylesLESS from "/imports/styles/index.less" // Error: Cannot find module '/imports/styles/index.less' on client
+import * as StylesLESS from "/imports/styles/index.less"
 import * as Stylus from "stylus"
 import * as StylesStylus from "/imports/styles/index.styl"
 
