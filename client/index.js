051a92f2c64c66dbd38fbba5a81eff7c9f0c7033
B.E. Henriksen <e@jiku.ca>
2016-01-30 15:10:23 +0100

    Tries to lazy-load (transpiled) stylesheets.

---
+++ client/index.js
@@ -0,0 +1,11 @@
+import * as StylesCSS from "/imports/styles/index.css"
+import * as StylesSASS from "/imports/styles/index.sass"
+import * as StylesLESS from "/imports/styles/index.less" // Error: Cannot find module '/imports/styles/index.less' on client
+import * as Stylus from "stylus"
+import * as StylesStylus from "/imports/styles/index.styl"
+
+/*
+Meteor.startup(function () {
+  // code to run on server at startup
+});
+*/
